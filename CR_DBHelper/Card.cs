342f013244ea488d90184a2f60298aa714e9878d
Petrik Diak <diak@petrik.hu>
2023-11-21 11:53:45 +0100

    Add project files.

---
+++ CR_DBHelper/Card.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR_DBHelper
+{
+	internal class Card
+	{
+		[JsonProperty(PropertyName = "id")]
+		public int Id { get; set; }
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+		[JsonProperty(PropertyName = "arena")]
+		public int Arena {get; set; }
+		[JsonProperty(PropertyName = "type")]
+		public string Type {get; set; }
+		[JsonProperty(PropertyName = "rarity")]
+		public string Rarity { get; set; }
+		[JsonProperty(PropertyName = "description")]
+		public string Description { get; set; }
+		[JsonProperty(PropertyName = "guessed")]
+		public bool Guessed { get; set; }
+	}
+}
---
+++ CR_DBHelper/CardService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR_DBHelper
+{
+	internal class CardService
+	{
+		HttpClient client = new HttpClient();
+		string url = "https://retoolapi.dev/yM3igV/people";
+		public List<Card> GetAll()
+		{
+			string json = client.GetStringAsync(url).Result;
+			return JsonConvert.DeserializeObject<List<Card>>(json);
+		}
+
+		public Card Add(Card card)
+		{
+			StringContent content = new StringContent(JsonConvert.SerializeObject(card), Encoding.UTF8, "application/Json");
+			HttpResponseMessage responseMessage = client.PostAsync(url, content).Result;
+			string responseContent = responseMessage.Content.ReadAsStringAsync().Result;
+			Debug.WriteLine(responseContent);
+			return JsonConvert.DeserializeObject<Card>(responseContent);
+		}
+
+		public bool Delete(string urlNew)
+		{
+			HttpResponseMessage response = (client.DeleteAsync(urlNew)).Result;
+			return response.IsSuccessStatusCode;
+		}
+
+		public Card Update(int id, Card card)
+		{
+			string urlNew = "https://retoolapi.dev/yM3igV/people/" + id;
+			StringContent content = new StringContent(JsonConvert.SerializeObject(card), Encoding.UTF8, "application/Json");
+			HttpResponseMessage response = (client.PatchAsync(urlNew, content)).Result;
+			string responseContent = response.Content.ReadAsStringAsync().Result;
+			Debug.WriteLine(responseContent);
+			return JsonConvert.DeserializeObject<Card>(responseContent);
+		}
+	}
+}
